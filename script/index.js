3bdb0fbd7503f60f32fec974139487200162da9e
Gustavo Beleze <Gustavo Beleze>
2023-06-01 15:19:47 -0300

    pags de cadastro e login diferentes funcionando

--- script/index.js
+++ script/index.js
@@ -8,30 +8,56 @@ let usuarios = [
 function clear() {
   document.getElementById("email").value = "";
   document.getElementById("password").value = "";
+  document.getElementById("emailCadastro").value = "";
+  document.getElementById("passwordCadastro").value = "";
+  document.getElementById("confirmPassword").value = "";
 }
 
 function cadastrar() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const novoUsuario = { email: email, senha: password };
+  const email = document.getElementById("emailCadastro").value;
+  const password = document.getElementById("passwordCadastro").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
 
-  usuarios.push(novoUsuario);
-
-  clear();
+  if (email && password) {
+    if (password === confirmPassword) {
+      const novoUsuario = { email: email, senha: password };
+      usuarios.push(novoUsuario);
+      clear();
+      alert("Novo usuário cadastrado!");
+      toggleDisplay();
+    } else {
+      alert("Senhas não são iguais!");
+      clear();
+    }
+  } else {
+    alert("Preencha os campos necessários!");
+  }
 }
 
 function validar() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const login = { email: email, senha: password };
-
   const check = usuarios.find((e) => e.email === email);
 
-  if (check.email === login.email && check.senha == login.senha) {
-    alert("Sucesso");
+  if (email && password) {
+    if (check !== undefined) {
+      if (check.email === login.email && check.senha == login.senha) {
+        alert("Sucesso");
+      } else {
+        alert("Senha inválida");
+      }
+    } else {
+      alert("E-mail não cadastrado!");
+    }
   } else {
-    alert("Login ou senha inválidos");
+    alert("Preencha os campos solicitados!");
   }
 
   clear();
+}
+
+function toggleDisplay() {
+  document.querySelector(".login-box").classList.toggle("display-none");
+  document.querySelector(".cadastro-box").classList.toggle("display-none");
 }
